181b4e3b4fdbd2dc04eee259f527fafa38b2051d
Mohamet <metzooba@gmail.com>
2024-04-28 16:41:11 +0200

    maj users.spec.js

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -26,7 +26,7 @@ describe("tester API users", () => {
 
   beforeEach(() => {
     token = jwt.sign({ userId: USER_ID }, config.secretJwtToken);
-    // mongoose.Query.prototype.find = jest.fn().mockResolvedValue(MOCK_DATA);
+    mongoose.Query.prototype.find = jest.fn().mockResolvedValue(MOCK_DATA);
     mockingoose(User).toReturn(MOCK_DATA, "find");
     mockingoose(User).toReturn(MOCK_DATA_CREATED, "save");
   });
